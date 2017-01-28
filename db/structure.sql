114501fc333538e53fa6ba6a282f922fbbc34fec
Michael Pope <map7@corsairsolutions.com.au>
2017-01-29 00:37:50 +1100

    Add pattern matching indexes

--- db/structure.sql
+++ db/structure.sql
@@ -179,6 +179,27 @@ CREATE UNIQUE INDEX index_customers_on_email ON customers USING btree (email);
 
 
 --
+-- Name: index_customers_on_lower_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_customers_on_lower_email ON customers USING btree (lower((email)::text));
+
+
+--
+-- Name: index_customers_on_lower_first_name_varchar_pattern_ops; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_customers_on_lower_first_name_varchar_pattern_ops ON customers USING btree (lower((first_name)::text) varchar_pattern_ops);
+
+
+--
+-- Name: index_customers_on_lower_last_name_varchar_pattern_ops; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_customers_on_lower_last_name_varchar_pattern_ops ON customers USING btree (lower((last_name)::text) varchar_pattern_ops);
+
+
+--
 -- Name: index_customers_on_username; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -208,6 +229,7 @@ SET search_path TO "$user",public;
 INSERT INTO schema_migrations (version) VALUES
 ('20170121054155'),
 ('20170126011611'),
-('20170128105621');
+('20170128105621'),
+('20170128133542');
 
 
