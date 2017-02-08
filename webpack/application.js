d76e69c786f4c0f5e2f884cdf75a70cd6bec1673
Michael Pope <map7@corsairsolutions.com.au>
2017-02-08 22:27:40 +1100

    Configure AngularJS

--- webpack/application.js
+++ webpack/application.js
@@ -1,3 +1,14 @@
 require("application.css");
 require("bootstrap/dist/css/bootstrap.css");
-console.log("Hello world!");
+var coreJS =              require("core-js");
+var zoneJS =              require("zone.js");
+var reflectMetadata =     require("reflect-metadata");
+var ng = {
+  core:                   require("@angular/core"),
+  common:                 require("@angular/common"),
+  compiler:               require("@angular/compiler"),
+  forms:                  require("@angular/forms"),
+  platformBrowser:        require("@angular/platform-browser"),
+  platformBrowserDynamic: require("@angular/platform-browser-dynamic"),
+  router:                 require("@angular/router")
+};
