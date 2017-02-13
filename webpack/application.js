c0c67d6bde14834453a39763ca9271a43e9008b5
Michael Pope <map7@corsairsolutions.com.au>
2017-02-13 22:11:14 +1100

    Iterate over an array of results

--- webpack/application.js
+++ webpack/application.js
@@ -14,7 +14,44 @@ var ng = {
   platformBrowserDynamic: require("@angular/platform-browser-dynamic"),
   router:                 require("@angular/router")
 };
-
+var RESULTS = [
+  {
+    first_name: "Pat",
+    last_name: "Smith",
+    username: "psmith",
+    email: "pat.smith@somewhere.net",
+    created_at: "2016-02-05"
+  },
+  {
+    first_name: "Patrick",
+    last_name: "Jones",
+    username: "pjpj",
+    email: "jones.p@business.net",
+    created_at: "2014-03-05"
+  },
+  {
+    first_name: "Patricia",
+    last_name: "Benjamin",
+    username: "pattyb",
+    email: "benjie@aol.info",
+    created_at: "2016-01-02"
+  },
+  {
+    first_name: "Patty",
+    last_name: "Patrickson",
+    username: "ppat",
+    email: "pppp@freemail.computer",
+    created_at: "2016-02-05"
+  },
+  {
+    first_name: "Jane",
+    last_name: "Patrick",
+    username: "janesays",
+    email: "janep@company.net",
+    created_at: "2013-01-05"
+  },
+];    
+ 
 // --------------------------------------------------------------------------------
 // AngularTestComponent
 // --------------------------------------------------------------------------------
@@ -89,27 +126,34 @@ var CustomerSearchComponent = ng.core.Component({
       <h1 class="h3">Results</h1> \
     </header> \
     <ol class="list-group"> \
-      <li class="list-group-item clearfix"> \
+      <li *ngFor="let customer of customers" \
+          class="list-group-item clearfix"> \
         <h3 class="pull-right"> \
           <small class="text-uppercase">Joined</small> \
-          2016-01-01\
+          {{customer.created_at}} \
         </h3> \
         <h2 class="h3"> \
-          Pat Smith \
-          <small>psmith34</small> \
+          {{customer.first_name}} {{customer.last_name}} \
+          <small>{{customer.username}}</small> \
         </h2> \
-        <h4>pat.smith@example.com</h4> \
+        <h4>{{customer.email}}</h4> \
       </li> \
     </ol> \
   </section> \
   '
 }).Class({
   constructor: function(){
-    this.keywords = null;
+    this.customers = null;
+    this.keywords = "";
   },
   
   search: function() {
-    alert("Searched for: " + this.keywords);
+    if (this.keywords == "pat"){
+      this.customers = RESULTS;
+    }
+    else {
+      this.customers = [];
+    }
   }
 });
 
