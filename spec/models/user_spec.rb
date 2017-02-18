ab42a5da46efffde1ffa4701fe7844de9a11c640
Michael Pope <map7@corsairsolutions.com.au>
2017-02-18 16:24:29 +1100

    Test-Database constraint

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe User do
+  describe "#email" do 
+    let(:user) {
+      User.create!(email: "foo@example.com",
+                   password: "qwertyuiop", password_confirmation: "qwertyuiop")
+    }
+    it "absolutely prevents invalid email addresses" do
+      expect {
+        user.update_attribute(:email, "foo@bar.com")
+      }.to raise_error(ActiveRecord::StatementInvalid,
+                       /email_must_be_company_email/i)
+    end
+  end
+end
