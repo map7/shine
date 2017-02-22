5175cf37e5b43f64c7e36b2ac5c1e6390b910d85
Michael Pope <map7@corsairsolutions.com.au>
2017-02-22 21:12:49 +1100

    Setup capybara to use poltergeist

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,11 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'capybara/poltergeist'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+Capybara.javascript_driver = :poltergeist
+Capybara.default_driver = :poltergeist
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
