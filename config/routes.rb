29c63204f70d17dcadab367aa6ef5ad59c8dc64b
Michael Pope <map7@corsairsolutions.com.au>
2017-03-14 22:17:30 +1100

    Add routes for AngularJS2 routes in Rails

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'dashboard#index'
+
+  # These supercede other /customers routes, so must come before resource :customers
+  get "customers/ng", to: "customers#ng"
+  get "customers/ng/*angular_route", to: "customers#ng#"
+  # the *angular_route is called route globbing, meaning it can be anything and will be put into params.
+  
   resources :customers, only: [:index]
   get "angular_test" => "angular_test#index"
 end
