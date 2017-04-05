93069e70517d922c60a0f4ef74ba0a8744f6c06a
Michael Pope <map7@corsairsolutions.com.au>
2017-04-05 22:39:01 +1000

    Require router in details

--- webpack/CustomerDetailsComponent.js
+++ webpack/CustomerDetailsComponent.js
@@ -1,14 +1,18 @@
 var reflectMetadata = require("reflect-metadata");
 var ng={
-  core: require("@angular/core")
+  core: require("@angular/core"),
+  router: require("@angular/router")
 };
+
 var CustomerDetailsComponent = ng.core.Component({
   selector: "shine-customer-details",
   template: require("./CustomerDetailsComponent.html")
 }).Class({
   constructor: [
-    function(){
-      
+    ng.router.ActivatedRoute,
+    function(activatedRoute){
+      this.activatedRoute = activatedRoute;
+      this.id = null;
     }
   ]
 });
