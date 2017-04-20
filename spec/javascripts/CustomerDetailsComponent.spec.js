558665412b2a9b6827f685fba8063264cbb7f507
Michael Pope <map7@corsairsolutions.com.au>
2017-04-20 21:26:42 +1000

    Create mockHttp

--- spec/javascripts/CustomerDetailsComponent.spec.js
+++ spec/javascripts/CustomerDetailsComponent.spec.js
@@ -11,6 +11,28 @@ var td = require("testdouble");
 
 var component = null;
 
+var createMockHttp = function(customer){
+  // Setup a response object for returning
+  var response = td.object(["json"]);
+  td.when(response.json()).thenReturn({ customer: customer});
+
+  // Create an obserable when we will use to watch for http requests
+  var observable = td.object(["subscribe"]);
+  td.when(observable.subscribe(
+    td.callback(response),
+    td.matchers.isA(Function))).thenReturn();
+
+  // Setup a mockHttp object
+  var mockHttp = td.object(["get"]);
+
+  // When there is a call to /customers/<id>.json return the observable above.
+  td.when(
+    mockHttp.get("/customers/" + customer.id + ".json")
+  ).thenReturn(observable);
+
+  return mockHttp;
+};
+
 describe("CustomerDetailsComponentComponent", function(){
   describe("initial state", function(){
     beforeEach(function(){
