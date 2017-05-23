9c13b58fd9be0b82bd6a6d5000f078c71d641088
Michael Pope <map7777@gmail.com>
2017-05-23 22:07:23 +1000

    Add some relationships to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,9 @@
 class Customer < ApplicationRecord
+  has_many :customers_shipping_addresses
+  has_one :customers_billing_addresses
+  has_one :billing_address, through: :customers_billing_addresses, source: :address
+
+  def primary_shipping_address
+    self.customers_shipping_address.find_by(primary: true).address
+  end
 end
