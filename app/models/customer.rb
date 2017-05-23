e7f6ce30cc9683e08d7cdb82483eccab86fd3200
Michael Pope <map7777@gmail.com>
2017-05-23 22:17:07 +1000

    Fix up relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ApplicationRecord
-  has_many :customers_shipping_addresses
-  has_one :customers_billing_addresses
+  has_many :customers_shipping_address
+  has_one :customers_billing_address
   has_one :billing_address, through: :customers_billing_addresses, source: :address
 
   def primary_shipping_address
